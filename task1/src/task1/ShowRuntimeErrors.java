da7ade4cc55a2c77fd086a6e8dac4937db8752ae
jiaojiaojiang <1611752939@qq.com>
2021-10-10 16:31:29 +0800

    task1

---
+++ task1/src/task1/ShowRuntimeErrors.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class ShowRuntimeErrors {
+	  public static void main(String[] args){
+		  System.out.println(1/0);
+	  }
+}
---
+++ task1/src/task1/Welcome.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class Welcome {
+  public static void main(String[] args){
+	  System.out.println("Welcome to Java!");
+  }
+}
